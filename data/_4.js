e3fc3e95bad343378c04847970ca1b7096a831dd
Arielmeroz <88772043+Arielmeroz@users.noreply.github.com>
2022-09-20 11:58:32 +0300

    Add files via upload

---
+++ data/_4.js
@@ -0,0 +1,8 @@
+var json__4 = {
+"type": "FeatureCollection",
+"name": "_4",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "Id": 0.0, "אזור": "אילת - אשקלון", "Field": "0", "שם המ�": "קו קצצא", "מיקום": "נגב - ערבה", "במהנא": null, "על מנ�": null, "מצב מ�": "חסוי", "פעילי": null, "סטטוס": "פעיל", "עמדת �": null, "ארגונ": null, "מהנדר": "0" }, "geometry": { "type": "MultiLineString", "coordinates": [ [ [ 34.962787406662351, 29.552774430929112 ], [ 34.787698124547603, 31.251556009118623 ], [ 34.557895267394407, 31.676252252339612 ] ] ] } }
+]
+}
